ba4517867c4652508a8934cbfd1273129ff5e992
Joan <joan.correo@gmail.com>
2024-01-28 11:47:41 +0100

    Update avr155.h
    Update avr155.h

--- sof/avr155.h
+++ sof/avr155.h
@@ -1,12 +1,14 @@
 #ifndef _AVR155_
 #define _AVR155_
 /*
-Protocolo es el NEC extendido
-Trama 	1 start bit 9ms + off 4.5ms + 4bytes datos (dos primeros codigo fabricante, byte 3 codigo y byte 4 codigo invertido)
+ El equipo AVR155 utiliza el protocolo es el NEC extendido para recibir ordenes a 
+ traves del mando distancia de infarrrojos.
+Los dos primeros bytes son el codigo fabricante, byte 3 codigo y byte 4 codigo 
+Trama 	1 start bit 9ms + off 4.5ms + 4bytes datos
 Bit a 0 	560µs pulso, 560µs pausa ->1.125ms
 Bit a 1 	560µs pulso, 1690µs pausa ->2,25ms
-repetición tecla espera de 50ms + 9ms pulso, 2.250ms pausa, 560µs pulso, ~100ms pausa
-periodo reposo entre transmisiones 50ms
+Repetición tecla: espera de 50ms + 9ms pulso, 2.250ms pausa, 560µs pulso, ~100ms pausa
+Periodo reposo entre transmisiones: 50ms
 
 Codigo envio al  microcontrolador orden ON (IC90 T5CC1 pin 35, R476)
                                         <-bit0   <-bit8  <-bit16  <-bit34
@@ -20,79 +22,95 @@ Codigo envio al  microcontrolador orden ON (IC90 T5CC1 pin 35, R476)
 const uint16_t direccion1 = 0x7080;                                                   // 0111000010000000
 const uint16_t direccion2 = 0x7282;                                                   // 0111001010000010
 
-const uint8_t CodeOn = 0xC0;
-const uint8_t CodeOff = 0x9F;
-const uint8_t codeMute = 0xC1;
-const uint8_t codeAvr = 0x35;
-const uint8_t codeDvd = 0xD0;
-const uint8_t codeCd = 0xC4;
-const uint8_t codeVid1 = 0x53;
-const uint8_t codeVid2 = 0xD3;
-const uint8_t codeVid3 = 0x73;
-const uint8_t codeHdmi1 = 0x4F;
-const uint8_t codeHdmi2 = 0xCF;
-const uint8_t codeHdm13 = 0x3F;
-const uint8_t codeAux = 0xB3;
-const uint8_t codeTape = 0x33;
-const uint8_t codeSleep = 0xDB;
-const uint8_t codeTest = 0x31;
-const uint8_t code6ch = 0xDB;
-const uint8_t codeSurr = 0x1A;
-const uint8_t codeOsd = 0x3A;
-const uint8_t codeIncvol = 0xE3;
-const uint8_t codeDecvol = 0x13;
-const uint8_t codeCh = 0xBA;
-const uint8_t codeSpkr = 0xCA;
-const uint8_t codeArriba = 0x71;
-const uint8_t codeAbajo = 0xF1;
-const uint8_t codeDerecha = 0x43;
-const uint8_t codeIzquierda = 0x83;
-const uint8_t codeOk = 0x21;
-const uint8_t codeDigital = 0xA2;
-const uint8_t codeDelay = 0x4A;
-const uint8_t code1 = 0x87;
-const uint8_t code2 = 0x88;
-const uint8_t code3 = 0x89;
-const uint8_t code4 = 0x8A;
-const uint8_t code5 = 0x8B;
-const uint8_t code6 = 0x8C;
-const uint8_t code7 = 0x8D;
-const uint8_t code8 = 0x8E;
-const uint8_t code9 = 0x9D;
-const uint8_t code0 = 0x9E;
-const uint8_t codeTunm = 0xC9;
-const uint8_t codeMen = 0x61;
-const uint8_t codeIncTuning = 0x21;
-const uint8_t codeDecTuning = 0xA1;
-const uint8_t codeDirect = 0xD9;
-const uint8_t codeBorrar = 0x9B;
-const uint8_t codeIncPreset = 0x0B;
-const uint8_t codeDecPreset = 0x8B;
-const uint8_t codeTone = 0xA3;
-const uint8_t codeRds = 0xBB;
-const uint8_t codeDolby = 0x0A;
-const uint8_t codeDts = 0x05;
-const uint8_t codeDtsNeo = 0x85;
-const uint8_t codeNoche = 0x69;
-const uint8_t codeLogic = 0x45;
-const uint8_t codeStereo = 0xD9;
-const uint8_t codeSkipSwn = 0x0C;
-const uint8_t codeSkipUp = 0x8C;
-const uint8_t codeDim = 0x3B;
+const uint8_t CodeOn = 0xC0;                                                          // direccion1
+const uint8_t CodeOff = 0x9F;                                                         // direccion1
+const uint8_t CodeMute = 0xC1;                                                        // direccion1
+const uint8_t CodeAvr = 0xC0;                                                         // direccion1
+const uint8_t CodeDvd = 0xD0;                                                         // direccion1
+const uint8_t CodeCd = 0xC4;                                                          // direccion1
+const uint8_t CodeVid1 = 0xCA;                                                        // direccion1
+const uint8_t CodeVid2 = 0xCB;                                                        // direccion1
+const uint8_t CodeVid3 = 0xCE;                                                        // direccion1
+const uint8_t CodeHdmi1 = 0xF2;                                                       // direccion1
+const uint8_t codeHdmi2 = 0xF3;                                                       // direccion1
+const uint8_t CodeHdmi3 = 0xFC;                                                       // direccion1
+const uint8_t CodeAux = 0xCD;                                                         // direccion1
+const uint8_t CodeTape = 0xCC;                                                        // direccion1
+const uint8_t CodeSleep = 0xDB;                                                       // direccion1
+const uint8_t CodeAmFm = 0x81;                                                        // direccion1
+const uint8_t CodeTest = 0x8C;                                                        // direccion1
+const uint8_t CodeIncvol = 0xC7;                                                      // direccion1
+const uint8_t CodeDecvol = 0xC8;                                                      // direccion1
+const uint8_t CodeSpkr = 0x53;                                                        // direccion1
+const uint8_t Code1 = 0x87;                                                           // direccion1
+const uint8_t Code2 = 0x88;                                                           // direccion1
+const uint8_t Code3 = 0x89;                                                           // direccion1
+const uint8_t Code4 = 0x8A;                                                           // direccion1
+const uint8_t Code5 = 0x8B;                                                           // direccion1
+const uint8_t Code6 = 0x8C;                                                           // direccion1
+const uint8_t Code7 = 0x8D;                                                           // direccion1
+const uint8_t Code8 = 0x8E;                                                           // direccion1
+const uint8_t Code9 = 0x9D;                                                           // direccion1
+const uint8_t Code0 = 0x9E;                                                           // direccion1
+const uint8_t CodeTunm = 0x93;                                                        // direccion1
+const uint8_t CodeMen = 0x86;                                                         // direccion1
+const uint8_t CodeIncTuning = 0x84;                                                   // direccion1
+const uint8_t CodeDecTuning = 0x85;                                                   // direccion1
+const uint8_t CodeDirect = 0x9B;                                                      // direccion1
+const uint8_t CodeDim = 0xDC;                                                         // direccion1
+
+const uint8_t CodeDts = 0xA0;                                                         // direccion2
+const uint8_t CodeDolby = 0x50;                                                       // direccion2
+const uint8_t CodeIncPreset = 0x0B;                                                   // direccion2
+const uint8_t CodeSkipSwn = 0x0C;                                                     // direccion2
+const uint8_t CodeSurr = 0x58;                                                        // direccion2
+const uint8_t CodeCh = 0x5D;                                                          // direccion2
+const uint8_t CodeOk = 0x84;                                                          // direccion2
+const uint8_t Code6ch = 0xDB;                                                         // direccion2
+const uint8_t CodeOsd = 0x5C;                                                         // direccion2
+const uint8_t CodeDerecha = 0xc2;                                                     // direccion2
+const uint8_t CodeLogic = 0xA2;                                                       // direccion2
+const uint8_t CodeDelay = 0xD9;                                                       // direccion2
+const uint8_t CodeNoche = 0x96;                                                       // direccion2
+const uint8_t CodeArriba = 0x99;                                                      // direccion2
+const uint8_t CodeIzquierda = 0xC1;                                                   // direccion2
+const uint8_t CodeDtsNeo = 0xA1;                                                      // direccion2
+const uint8_t CodeDecPreset = 0x8B;                                                   // direccion2
+const uint8_t CodeSkipUp = 0x8C;                                                      // direccion2
+const uint8_t CodeBorrar = 0x9B;                                                      // direccion2
+const uint8_t CodeDigital = 0x54;                                                     // direccion2
+const uint8_t CodeTone = 0xC5;                                                        // direccion2
+const uint8_t CodeRds = 0xDD;                                                         // direccion2
+const uint8_t CodeStereo = 0x9B;                                                      // direccion2
+const uint8_t CodeAbajo = 0x9A;                                                       // direccion2
 
 // Envio de 32 bytes al microcontrolador T5CC1 del AVR155
-void CodeAvrTX (bool adders, uint8_t command) {
+void CodeAvrTX (bool adders, uint8_t command, uint8_t repit) {
 uint8_t i, x = 0;
 uint16_t ini;
-    adders ? ini = direccion2: ini = direccion1;
-    pinMode(DataPin, OUTPUT_OPEN_DRAIN);                                              // pin como salida
+
+#if DebugSerial > 1
+    Serial.print("Enviar datos a cpu AVR155, direccion: ");
+    Serial.print(adders);
+    Serial.print(" orden: ");
+    Serial.print(command);
+    Serial.print(" repeticiones: ");
+    Serial.println(repit);
+#endif
+    adders ? ini = direccion2 : ini = direccion1;
+    digitalWrite(ControlPin, HIGH);                                                   // Desconectamos señal receptor infrarrojos a CPU AVR155
+#if StatusLed
+    digitalWrite(LedPin, HIGH);
+#endif
+    //pinMode(DataPin, OUTPUT_OPEN_DRAIN);                                            // pin como salida (colector abierto)
+    pinMode(DataPin, OUTPUT);
     digitalWrite(DataPin, LOW);
-    delayMicroseconds(9000);                                                          // 9ms a 0
+    delayMicroseconds(9000); // 9ms a 0
     digitalWrite(DataPin, HIGH);
-    delayMicroseconds(4500);                                                          // 4.5ms a 1
+    delayMicroseconds(4500); // 4.5ms a 1
     for (i = 0; i < 16; i++) {
         digitalWrite(DataPin, LOW);
-        delayMicroseconds(560);                                                       // 560us a 0
+        delayMicroseconds(560); // 560us a 0
         digitalWrite(DataPin, HIGH);
         bitRead(ini, i) ? delayMicroseconds(1690) : delayMicroseconds(560);           // 1->1690us 0->560us a 1
     }
@@ -108,7 +126,23 @@ uint16_t ini;
     } while (++x == 1);
     delayMicroseconds(560);
     digitalWrite(DataPin, HIGH);
+    if (repit != 0) {
+        for (i = 0; i < repit; i++) {
+            delay (50);                                                               // 50ms
+            digitalWrite(DataPin, LOW);
+            delayMicroseconds(9000);                                                  // 9ms a 0
+            digitalWrite(DataPin, HIGH);
+            delayMicroseconds(2250);                                                  // 2.250us a 1
+            digitalWrite(DataPin, LOW);
+            delayMicroseconds(560);                                                   // 560us a 0
+            digitalWrite(DataPin, HIGH);
+            delay(100);      
+        }
+    }
+#if StatusLed
+    digitalWrite(LedPin, LOW);
+#endif
     pinMode(DataPin, INPUT);
+    digitalWrite(ControlPin, LOW);                                                    // Conectamos señal receptor infrarrojos a CPU AVR155
 }
-
 #endif
